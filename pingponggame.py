dc5c414aca9d359184c479fa0b1da1793b631fb6
Ken Ji <85244798+Kenjizard@users.noreply.github.com>
2022-03-27 15:27:26 +0800

    Add files via upload

--- pingponggame.py
+++ pingponggame.py
@@ -1,3 +1,4 @@
+
 from pygame import *
 from random import randint
 
@@ -17,24 +18,78 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-        self.seed = player_speed
+        self.speed = player_speed
 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    pass
+    #control function
+    def update_left(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+
+    def update_right(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
 
 white = (255, 255, 255)
-pad_1 = Player()
-pad_2 = Player()
-ball = GameSprite()
+pad_1 = Player('C:\\Users\\kenji\\Downloads\\Pad_1.png',30,200,4,50,150,white)
+pad_2 = Player('C:\\Users\\kenji\\Downloads\\Pad_2.png',520,200,4,50,150,white)
+ball = GameSprite('C:\\Users\\kenji\\Downloads\\ball.png',200,200,4,50,50,white)
 
 game = True
+finish = False
+
+speed_x = 3
+speed_y = 3
+
+clock = time.Clock()
+FPS = 60
+
+font.init()
+font = font.Font(None, 45)
+lose1 = font.render('Player 1 Lose!', True, (180, 0, 0))
+lose2 = font.render('Player 2 Lose!', True, (180, 0, 0))
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
 
+    if finish != True:
+        window.fill(back)
+        pad_1.update_left()
+        pad_2.update_right()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        #reaction (ball)
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+        
+        if sprite.collide_rect(pad_1, ball) or sprite.collide_rect(pad_2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200,200))
+        
+        if ball.rect.x > win_width - 50:
+            finish = True
+            window.blit(lose2, (200,200))
+
+        pad_1.reset()
+        pad_2.reset()
+        ball.reset()
+            
+
     display.update()
+    clock.tick(FPS)
